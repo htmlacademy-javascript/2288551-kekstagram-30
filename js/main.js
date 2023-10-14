e27af7c6f7ad0b608528d17e9a52f2cab103ac8f
Elvira Fatin <elvirrrk@gmail.com>
2023-10-14 21:40:00 +0300

    feat: add functions in main.js

--- js/main.js
+++ js/main.js
@@ -31,22 +31,54 @@ const MESSAGE = [
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.'
 ];
 
+
+const createIdGenerator = function () {
+  let lastGeneratedId = 0;
+  return function () {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+};
+
 function getRandomInteger (min, max) {
-  let lower = Math.ceil(Math.min(min, max));
-  let upper = Math.floor(Math.max(min,max));
-  let result = Math.random() * (upper - lower + 1) + lower;
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min,max));
+  const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 }
 
-const photo = {
-  id: '',
-  url: 'photos/{{i}}.jpg, i от 1 до 25, не должны повторяться',
-  description: '',
+function createRandomIdFromRangeGenerator (min, max) {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+const commentId = createIdGenerator();
+const photoId = createRandomIdFromRangeGenerator(1, OBJECT_COUNT);
+const photoUrl = createRandomIdFromRangeGenerator(1, OBJECT_COUNT);
+
+const photo = () => ({
+  id: photoId,
+  url: `photos/${photoUrl}.jpg`,
+  description: getRandomArrayElement(DESCRIPTION),
   likes: getRandomInteger(15,200),
   comments: {
-    id: 'любое число. не должны повторяться',
-    avatar: 'формируется по правилу img/avatar-{{случайное число от 1 до 6}}.svg',
-    message: '',
-    name: '',
+    id: commentId,
+    avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+    message: getRandomArrayElement(MESSAGE),
+    name: getRandomArrayElement(NAME),
   }
-};
+});
+// нужна функция генерации comments 0 - 30
