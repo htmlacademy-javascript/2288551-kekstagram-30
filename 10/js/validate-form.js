1fdf2f5c9b7fc6c3a1830b9ca74d7a6e8a453ec7
Keks <keks@htmlacademy.ru>
2023-11-08 17:54:17 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/validate-form.js
@@ -0,0 +1,111 @@
+import { isEscapeKey } from './util';
+import { addEffects, removeEffects } from './apply-effect';
+
+const form = document.querySelector('.img-upload__form');
+const imgUploadInput = document.querySelector('#upload-file');
+const imgUploadOverlay = document.querySelector('.img-upload__overlay');
+const imgUploadPreview = document.querySelector('.img-upload__preview img'); //туда надо положить добавленное фото
+const buttonCancel = document.querySelector('#upload-cancel');
+const textDescription = document.querySelector('.text__description');
+const textHashtags = document.querySelector('.text__hashtags');
+const hashtagRegExp = /^#[a-zа-яё0-9]{1,19}$/i;
+const HASHTAGS_COUNT = 5;
+
+const onDocumentKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeForm(); //другая ф-ция, нельзя взять из render-big-picture
+  }
+};
+
+const onTextKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.stopPropagation();
+  }
+};
+
+//создаем валидацию
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'form-error'
+});
+
+const onValidation = (evt) => {
+  evt.preventDefault();
+  const isValid = pristine.validate();
+  if (isValid) {
+    form.submit();
+  }
+};
+
+function openForm() {
+  document.body.classList.add('modal-open');
+  imgUploadOverlay.classList.remove('hidden');
+  addEffects();
+  form.addEventListener('submit', onValidation);
+  document.addEventListener('keydown', onDocumentKeydown);
+  textDescription.addEventListener('keydown', onTextKeydown);
+  textHashtags.addEventListener('keydown', onTextKeydown);
+}
+
+buttonCancel.addEventListener('click', () => {
+  closeForm();
+});
+
+function closeForm() {
+  form.reset();
+  removeEffects();
+  form.removeEventListener('submit',onValidation);
+  document.body.classList.remove('modal-open');
+  imgUploadOverlay.classList.add('hidden');
+  document.removeEventListener('keydown', onDocumentKeydown);
+  textDescription.removeEventListener('keydown', onTextKeydown);
+  textHashtags.removeEventListener('keydown', onTextKeydown);
+}
+
+//добавить загруженное фото
+//  imgUploadPreview.addEventListener('load', (evt) => {
+//   imgUploadPreview.src = evt.target.result;
+// });
+
+//количество хэш-тегов не более 5
+function checkHashtagCount(arrayString) {
+  return (arrayString.length <= HASHTAGS_COUNT);
+}
+
+//хэш-тег соответсвует регулярному выражению
+function checkRegExp(arrayString) {
+  return arrayString.every((hashtag) => hashtagRegExp.test(hashtag));
+}
+
+//хэш-тег уникален
+function checkUnique(arrayString) {
+  return (new Set(arrayString).size === arrayString.length);
+}
+
+const validator = (type) => (string) => {
+  if (string.length === 0) { //если string пустая не проверять
+    return true;
+  }
+  const arrayString = (string.toLowerCase().trim()).split(' ').filter((tag) => Boolean(tag.length));
+
+  switch (type) {
+    case 'count':
+      return checkHashtagCount(arrayString);
+    case 'regExp':
+      return checkRegExp(arrayString);
+    case 'unique':
+      return checkUnique(arrayString);
+  }
+};
+
+pristine.addValidator(textHashtags, validator('count'), 'не более 5 хэш-тегов');
+pristine.addValidator(textHashtags, validator('regExp'), 'некорректно введен хэш-тег');
+pristine.addValidator(textHashtags, validator('unique'), 'хэш-теги не должны повторяться');
+
+function uploadImage() {
+  imgUploadInput.addEventListener('change', openForm);
+}
+
+export { uploadImage, imgUploadPreview };
